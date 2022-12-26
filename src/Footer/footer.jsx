873297ecbf9612342a35d7062df433407af7889e
Jonathan Ikpen <jonathanikpen4@gmail.com>
2022-12-26 01:03:38 +0100

    feat: add twitter account

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -1,7 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="mt-20 p-6 pb-12 w-full flex justify-center items-center bg-white text-[16px] text-black-3">
-      copyright &copy; 2022-2023 Luca Inc. All rights reserved.
+    <footer className="mt-20 p-6 pb-12 w-full flex flex-col justify-center items-center bg-white text-[16px] text-black-3">
+      <div>
+        <span className="font-bold">Socials:</span>{" "}
+        <a
+          href="https://twitter.com/paywithluca"
+          className="underline hover:no-underline"
+          target="_blank"
+        >
+          twitter
+        </a>
+      </div>
+      <div>copyright &copy; 2022-2023 Luca Inc. All rights reserved.</div>
     </footer>
   );
 };
